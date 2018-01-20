951502036365b831629f80763300bc070396df91
Qemwen <github@myfirstfuture.com>
2018-01-20 20:00:13 +0100

    Initial commit

---
+++ desktop/src/com/sheepgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sheepgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sheepgame.SheepStorm;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SheepStorm(), config);
+	}
+}
